794dcbf5e33f44c38f6327de6230ddcf88be87ef
Lucas Henrique <lucashfsdev@gmail.com>
2025-04-17 11:12:54 -0300

    fix: corrected bank access.

--- src/test/core/user.test.ts
+++ src/test/core/user.test.ts
@@ -1,4 +1,27 @@
 import request from "supertest";
 import app from "../../app";
+import db from "../../config/db";
+import mongoose from "mongoose";
 
-describe("GET ", async () => {});
+let token: string;
+
+beforeAll(async () => {
+  console.log("Cheguei aqui");
+  await db();
+  const response = await request(app)
+    .post("/login")
+    .send({ email: "admin@admin.com", password: "admin" });
+
+  token = response.body.accessToken;
+}, 10000);
+
+describe("[GET] /user", () => {
+  it("Find All Users", async () => {
+    const response = await request(app)
+      .get("/user")
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body.data)).toBe(true);
+  });
+});
